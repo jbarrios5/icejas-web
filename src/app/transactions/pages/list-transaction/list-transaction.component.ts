8060cba29ac301228049ce57ec1c1d3af4565753
juan barrios <jbarrios@roshka.com>
2024-02-13 08:45:27 -0300

    Se agrega llamada para obtener tipo de actividades

--- src/app/transactions/pages/list-transaction/list-transaction.component.ts
+++ src/app/transactions/pages/list-transaction/list-transaction.component.ts
@@ -3,14 +3,15 @@ import { Church } from '../../interface/church.interface';
 import { TransactionService } from '../../service/transactions.service';
 import Swal from 'sweetalert2';
 import { FormControl, FormGroup } from '@angular/forms';
+import { TransactionType, TransactionTypePosResData } from '../../interface/transaction.interface';
 export interface TransactionElement {
   date: string;
-  id:number;
+  id: number;
   details: string;
-  type:string;
+  type: string;
   credit: number;
   debit: number;
-  balance:number;
+  balance: number;
 
 
 }
@@ -24,64 +25,81 @@ export interface TransactionElement {
   templateUrl: './list-transaction.component.html',
   styleUrls: ['./list-transaction.component.css']
 })
-export class ListTransactionComponent implements OnInit{
+export class ListTransactionComponent implements OnInit {
   ELEMENT_DATA: TransactionElement[] = [];
-  displayedColumns: string[] = ['fecha','Nro', 'actividad','observacion', 'ingreso', 'egreso','saldo'];
-  dataSource: TransactionElement [] = [];
-  
+  displayedColumns: string[] = ['fecha', 'Nro', 'actividad', 'observacion', 'ingreso', 'egreso', 'saldo'];
+  dataSource: TransactionElement[] = [];
+  types: TransactionType[] = [];
+
   public transactionListForm = new FormGroup({
-    transactionActivite: new FormControl('',{nonNullable:true}),
-    transactionType: new FormControl('',{nonNullable:true}),
-    transactionDateStart: new FormControl('',{nonNullable:true}),
-    transactionDateEnd: new FormControl('',{nonNullable:true})
+    transactionActivite: new FormControl('', { nonNullable: true }),
+    transactionType: new FormControl('', { nonNullable: true }),
+    transactionDateStart: new FormControl('', { nonNullable: true }),
+    transactionDateEnd: new FormControl('', { nonNullable: true })
   })
 
 
-  constructor(private transactionService:TransactionService){}
+  constructor(private transactionService: TransactionService) { }
 
 
   ngOnInit(): void {
     this.getTranasctionDetails();
+    this.getTypes();
   }
 
-  getTranasctionDetails():void{
-    let church:Church = JSON.parse(localStorage.getItem("church")!)
+  getTranasctionDetails(): void {
+    let church: Church = JSON.parse(localStorage.getItem("church")!)
     this.transactionService.getTransactionDetails(church.id)
-    .subscribe(res => {
+      .subscribe(res => {
 
-      if(!res){
-        Swal.fire('Error', 'Error inesperado', 'error')
-        return 
-      }
-      console.log(res);
-      
-      res.data.details.forEach( data => {
-        let trElement :TransactionElement = {
-          id: data.transactionId,
-          balance: data.currentBalance,
-          date: data.registeredDate,
-          details: data.transactionDetail,
-          type: data.transactionTypeName,
-          credit: data.transactionCategory === 'C'? data.amount:0,
-          debit: data.transactionCategory === 'D'? data.amount : 0,
+        if (!res) {
+          Swal.fire('Error', 'Error inesperado', 'error')
+          return
         }
-        this.ELEMENT_DATA.push(trElement);
-        
-      })
+        console.log(res);
 
-      this.addData()
-    }
-    ) 
-    
-    
+        res.data.details.forEach(data => {
+          let trElement: TransactionElement = {
+            id: data.transactionId,
+            balance: data.currentBalance,
+            date: data.registeredDate,
+            details: data.transactionDetail,
+            type: data.transactionTypeName,
+            credit: data.transactionCategory === 'C' ? data.amount : 0,
+            debit: data.transactionCategory === 'D' ? data.amount : 0,
+          }
+          this.ELEMENT_DATA.push(trElement);
+
+        })
+
+        this.addData()
+      }
+      )
+
+
   }
 
-  addData():void{
+  addData(): void {
     this.dataSource = this.ELEMENT_DATA
-    
+
   }
-  filter():void{
-    const {transactionDateStart,transactionDateEnd,transactionActivite,transactionType} = this.transactionListForm.value;
-    console.log(transactionDateStart,transactionDateEnd);
+  filter(): void {
+    const { transactionDateStart, transactionDateEnd, transactionActivite, transactionType } = this.transactionListForm.value;
+    console.log(transactionDateStart, transactionDateEnd);
+  }
+
+  getTypes(): void {
+    if (this.transactionService.transactionTypes.length === 0) {
+      this.transactionService.getTransactionTypes()
+        .subscribe((res: TransactionTypePosResData) => {
+          res.data.transactionTypes.forEach(tr => this.types.push(tr));
+          console.log(this.types);
+        }
+      )
+    }
+    else{
+      this.types = this.transactionService.transactionTypes;
+    }
+
   }
 }
--- src/app/transactions/service/transactions.service.ts
+++ src/app/transactions/service/transactions.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { map, Observable, tap } from "rxjs";
 import { environments } from "src/app/environments/environments";
 import { Transaction, TransactionDetailGetResData, TransactionPostReqData, TransactionPostResData, TransactionType, TransactionTypePosResData } from "../interface/transaction.interface";
 
@@ -8,21 +8,30 @@ import { Transaction, TransactionDetailGetResData, TransactionPostReqData, Trans
     providedIn: 'root'
   })
 export class TransactionService{
-    constructor(private httpClient:HttpClient){}
     
-    getTransactionTypes():Observable<TransactionTypePosResData>{
-        return this.httpClient.get<TransactionTypePosResData>(`${environments.icejasBaseUrl}/types`)
-    }
+  public transactionTypes:TransactionType[] = []
+  
+  constructor(private httpClient:HttpClient){}
+  
+  getTransactionTypes():Observable<TransactionTypePosResData >{
+      return this.httpClient.get<TransactionTypePosResData>(`${environments.icejasBaseUrl}/types`)
+      .pipe(
+        tap( res => this.transactionTypes = res.data.transactionTypes)
+      )
 
-    addTransaction(transactionPostReqData:TransactionPostReqData):Observable<TransactionPostResData>{
-        const header = new HttpHeaders(
-            {
-              'Content-Type': 'application/json'
-           
-            }
-          )
-        return this.httpClient.post<TransactionPostResData>(`${environments.icejasBaseUrl}/`,transactionPostReqData,{headers:header})
-    }
+      
+     
+  }
+
+  addTransaction(transactionPostReqData:TransactionPostReqData):Observable<TransactionPostResData>{
+      const header = new HttpHeaders(
+          {
+            'Content-Type': 'application/json'
+          
+          }
+        )
+      return this.httpClient.post<TransactionPostResData>(`${environments.icejasBaseUrl}/`,transactionPostReqData,{headers:header})
+  }
 
     getTransactionDetails(churchId:number):Observable<TransactionDetailGetResData>{
       return this.httpClient.get<TransactionDetailGetResData>(`${environments.icejasBaseUrl}/?church_id=${churchId}`);
