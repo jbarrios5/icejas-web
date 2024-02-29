91118535e5069fb2c3fcbcd21b0a0fd97232d3c0
juan barrios <jbarrios@roshka.com>
2024-02-29 20:56:37 -0300

    Se agrega tokens en la llamada a los mcs

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -10,16 +10,23 @@ import { environments } from "../../environments/environments";
 export class AuthService {
 
   constructor(private httpClient:HttpClient) { }
-
+  public accessToken:string = '';
   login(request:AuthPostReqData):Observable<AuthPostResData | undefined>{
     const URL = `${environments.authBaseUrl}/login`;
-    const header = new HttpHeaders(
+    const headers = new HttpHeaders(
       {
         'Content-Type': 'application/json',
-        'api_key': "apke"
+        'apiKey': "icejas-088cad7b-7a3b-4a04-b0c5-f4b0796e5b89",
+        
       }
     )
-    return this.httpClient.post<AuthPostResData>(URL,request,{headers:header})
+    request.data.password = btoa(request.data.password)
+    return this.httpClient.post<AuthPostResData>(URL,request,{headers:headers})
+    .pipe(
+      tap(
+        res => this.accessToken = res.data.login.accessToken
+      )
+    )
     
     
   }
